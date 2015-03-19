b2889c4af3cf727885a6f7cf7b36e95b143b5449
Mino Togna <Cosimo.Togna@Fao.org>
2015-03-19 18:50:50 +0700

    changed font to Roboto

--- base/src/main/resources/nfms/modules/layer-list.js
+++ base/src/main/resources/nfms/modules/layer-list.js
@@ -117,12 +117,8 @@ define([ "jquery", "message-bus", "layer-list-selector", "i18n", "jquery-ui", "f
 			
 			var panelBody = $( '<div class="panel-body group-panel-body no-padding" />' );
 			content.append( panelBody );
-//			panelBody.html("aaaaaaa");
-//			<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-//		      <div class="panel-body">
-//		        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-//		      </div>
-//		    </div>
+			
+//			panelBody.append( '<div class="row separator"><div class="col-md-10 col-md-offset-1"><hr></div></div>' );
 			
 			
 			
@@ -146,7 +142,8 @@ define([ "jquery", "message-bus", "layer-list-selector", "i18n", "jquery-ui", "f
 			var row					= $( '<div class="row row-layer no-margin no-padding" />' );
 			row.attr( 'id' , layerRowPrefix + portalLayer.id );
 			groupContainerBody.append( row );
-			
+//			row.insertBefore( $(":last-child", groupContainerBody) );
+//			row.insertBefore( groupContainerBody.find(".separator") );
 			var settings				= $( '<div class="col-md-1 no-padding settings" />' );
 			row.append( settings );
 			
