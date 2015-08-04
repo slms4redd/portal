4b385bdde68046a5e790584a1bbabf86e3b7e912
Mino Togna <Cosimo.Togna@Fao.org>
2015-08-04 10:37:34 +0700

    added configurable banner

--- base/src/main/resources/nfms/modules/banner.js
+++ base/src/main/resources/nfms/modules/banner.js
@@ -1,18 +1,49 @@
 define([ "jquery", "layout", "i18n", "message-bus", "module" ], function($, layout, i18n, bus, module) {
 
-//	var config = module.config();
-//
-//	if (!config["hide"]) {
-//		var divBanner = $("<div/>").attr("id", "banner");
-//		layout.header.prepend(divBanner);
-//
-//		if (config["show-flag"]) {
-//			$("<div/>").attr("id", "flag").appendTo(divBanner);
-//		}
-//		if (config["show-logos"]) {
-//			$("<div/>").attr("id", "logos").appendTo(divBanner);
-//		}
-//		$("<span/>").attr("id", "title").html(i18n["title"]).appendTo(divBanner);
-//		$("<div/>").attr("id", "banner-izq").appendTo(divBanner);
-//	}
+	var config = module.config();
+	
+	var addElements 	= function( parent , elements ){
+		$.each( elements , function( i , element ){
+			console.log( element );
+			
+			var domElem = $( '<' + element.name + '/>' );
+			
+			if( element.styles ){
+				for (var name in element.styles) {
+			        if ( element.styles.hasOwnProperty( name) ) {
+			        	domElem.css( name , element.styles[name] );
+			        }
+			    }
+			}
+			
+			if( element.attributes ){
+				for (var name in element.attributes) {
+			        if ( element.attributes.hasOwnProperty( name) ) {
+			        	domElem.attr( name , element.attributes[name] );
+			        }
+			    }
+			}
+			
+			if( element.label ){
+				domElem.append( i18n[element.label] );
+			}
+			
+			parent.append( domElem );
+			
+			if( element.elements ){
+				addElements( domElem, element.elements);
+			}
+		});
+	}
+	
+	if( config.elements ){
+		var bannerRow = $( '<div class="row banner"></div>' ); 
+		layout.header.append( bannerRow );
+		
+		var banner = $( '<div class="col-md-12"></div>' ); 
+		bannerRow.append( banner );
+		
+		addElements( banner, config.elements);
+	}
+	
 });
